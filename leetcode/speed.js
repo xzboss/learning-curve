36bafb4ea47e488b22ab6f2ca963e3c92a55de55
徐洲 <xuzhou@changba.com>
2024-01-09 17:08:14 +0800

    feat: 🎸 有效的字母异位词

--- leetcode/speed.js
+++ leetcode/speed.js
@@ -4,29 +4,29 @@ function test(func) {
   const end = new Date().getTime()
   console.log(end - start)
 }
-//speed fast -> slow
+// speed fast -> slow
+test(() => {
+  const arr = new Array(10000000).fill('0')
+})
+test(() => {
+  const arr = new Array(10000000)
+  for (let i = 0; i < 10000000; i++) {
+    arr[i] = 0
+  }
+})
 // test(() => {
 //   const arr = new Array(10000000).fill('0')
+//   for (let i = 0; i < 1000000; i++) {
+//     arr[i] = i
+//   }
+//   arr.indexOf(999999)
 // })
 // test(() => {
-//   const map = {}
-//   for (let i = 0; i < 10000000; i++) {
-//     map[i] = 0
+//   const arr = new Array(10000000).fill('0')
+//   for (let i = 0; i < 1000000; i++) {
+//     arr[i] = i
+//   }
+//   for (let i = 0; i < 1000000; i++) {
+//     if (arr[i] === 999999) break
 //   }
 // })
-test(() => {
-  const arr = new Array(10000000).fill('0')
-  for (let i = 0; i < 1000000; i++) {
-    arr[i] = i
-  }
-  arr.indexOf(999999)
-})
-test(() => {
-  const arr = new Array(10000000).fill('0')
-  for (let i = 0; i < 1000000; i++) {
-    arr[i] = i
-  }
-  for (let i = 0; i < 1000000; i++) {
-    if (arr[i] === 999999) break
-  }
-})
