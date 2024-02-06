2f09fbbdf65759ec8d40d10ce5685dd7ec9c581d
xzboss <942875315@qq.com>
2024-02-06 10:49:47 +0800

    fix: 🐛 fix

---
+++ leetcode/medium/028-两数相加.js
@@ -0,0 +1,53 @@
+
+/**
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ */
+
+
+
+/**
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+示例 2：
+
+输入：l1 = [0], l2 = [0]
+输出：[0]
+示例 3：
+
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1]
+ */
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+
+};
+
+
+
+
+const node1 = new ListNode(1)
+const node2 = new ListNode(2)
+const node3 = new ListNode(3)
+const node4 = new ListNode(4)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node1
+console.log(addTwoNumbers(node1))
+
+//module.exports = 
