a8771f1dfee078ecfe073def289ce9abc77003d8
xzboss <942875315@qq.com>
2024-02-07 09:26:03 +0800

    feat: 🎸 合并两个有序链表

--- leetcode/easy/035-合并两个有序链表.js
+++ leetcode/easy/035-合并两个有序链表.js
@@ -7,9 +7,9 @@
 
 
 
-function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+function ListNode (val, next) {
+	this.val = (val === undefined ? 0 : val)
+	this.next = (next === undefined ? null : next)
 }
 
 /**
@@ -17,24 +17,66 @@ function ListNode(val, next) {
  * @param {ListNode} list2
  * @return {ListNode}
  */
+// 普通方法思路：遍历第一个最小的链表，第二个链表进行插入
+// var mergeTwoLists = function (list1, list2) {
+// 	if (!list1 || !list2) {
+// 		return list1 || list2
+// 	}
+// 	if (list1.val > list2.val) {
+// 		[list1, list2] = [list2, list1]
+// 	}
+// 	//list1 为min
+// 	const head = list1
+// 	while (list1 && list2) {
+// 		if (list1.next === null) {
+// 			list1.next = list2
+// 			break
+// 		}
+// 		if (list1.val <= list2.val && list1.next.val >= list2.val) {
+// 			const temp = list2
+// 			list2 = list2.next
+// 			temp.next = list1.next
+// 			list1.next = temp
+// 		}
+// 		list1 = list1.next
+// 	}
+// 	return head
+// };
+
+// // 别人的普通
+// var mergeTwoLists = function (list1, list2) {
+// 	const head = {}
+// 	let pre = head
+// 	while(list1 && list2){
+// 		if(list1.val <= list2.val){
+// 			pre.next = list1
+// 			list1 = list1.next
+// 		}else {
+// 			pre.next = list2
+// 			list2 = list2.next
+// 		}
+// 		pre = pre.next
+// 	}
+// 	pre.next = list1 || list2
+// 	return head.next
+// };
+
+
+// 递归
 var mergeTwoLists = function (list1, list2) {
-	if (!list1 || !list2) {
+	if(list1 === null || list2 === null){
 		return list1 || list2
 	}
-	const head = list1
-	while (list1 || list2) {
-		if (list1.val <= list2.val && list1.next.val >= list2.val) {
-			const temp = list2
-			list2 = list2.next
-			temp.next = list1.next
-			list1.next = temp
-		}
-		list1 = list1.next
+	if(list1.val <= list2.val){
+		list1.next = mergeTwoLists(list1.next, list2)
+		return list1
+	}else {
+		list2.next = mergeTwoLists(list1, list2.next)
+		return list2
 	}
-	return head
 };
 
-const node1 = new ListNode(1)
+const node1 = new ListNode(2)
 const node2 = new ListNode(2)
 const node3 = new ListNode(4)
 
