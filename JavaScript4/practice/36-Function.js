767d6dbee8828d209bc6108b4d11928494c69dcd
xuz <xuz@wanthings.com>
2024-05-23 17:24:50 +0800

    feat: testfunction

--- JavaScript4/practice/36-Function.js
+++ JavaScript4/practice/36-Function.js
@@ -1,7 +1,64 @@
-'use strict'
-function foo(a,b){
-    console.log(b)
-    arguments[1] = 99
-    console.log(b)
+// function func(){
+//     foo()
+// }
+// const obj = {
+//     foo(){
+//         console.log(foo.caller)
+//     }
+// }
+// function foo(){
+//     console.log(foo.caller)
+// }
+// foo()
+// foo()
+// fun() // ReferenceError: fun is not defined
+// {
+//   function foo(){ }
+//   var fun = function(){ }
+// }
+// 'use strict'
+// function dfs(x){
+//   return dfs(x + 1)
+// }
+// dfs(1)
+
+// const overall = 'overall'
+// function container(){
+//   const x = 'x'
+//   function inner(){
+//     const y = 'y'
+//     return function content(){
+//       const z = 'z'
+//     }
+//   }
+//   return inner()
+// }
+// console.dir(container())
+
+// // const overall = 'overall'
+// // function container(){
+// //   const x = 'x'
+// //   function inner(){
+// //     const y = 'y'
+// //     return function content(){
+// //       const z = 'z'
+// //       const closure = x + y
+// //     }
+// //   }
+// //   return inner()
+// // }
+// // console.dir(container())
+
+// (function(){
+//   foo = function(){
+
+//   }
+// }
+  
+// )()
+// console.log(foo)
+const foo = ()=>{
+
 }
-foo(1,2)
+
+console.log(function(){}.name === '')
