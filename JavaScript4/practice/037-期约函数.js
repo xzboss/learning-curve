2a4cbfd4288f2f1761882dbdf8f91b8afa405861
xuz <xuz@wanthings.com>
2024-06-04 11:39:56 +0800

    feat: promiseA+部分

--- JavaScript4/practice/037-期约函数.js
+++ JavaScript4/practice/037-期约函数.js
@@ -47,37 +47,110 @@
 //   console.log(result)
 // })
 
-class ProgressPromise extends Promise{
-  constructor(executor) {
-    const notifyHandlers = []
-    super((resolve, reject) => {
-       return executor(resolve, reject, progress => {
-        notifyHandlers.forEach(notify => {
-          notify(progress)
-        })
-      })
-    })
-    this.notifyHandlers = notifyHandlers
-  }
-  notify = function (fun) {
-    this.notifyHandlers.push(fun)
-    return this
-  }
-}
-const p = new ProgressPromise((resolve, reject, notify) => {
-  function getData(step) {
-    if (step < 5) {
-      notify(`${step * 20} %.........`)
-      setTimeout(getData, 1000, step + 1)
-    } else {
-      resolve('done')
-    }
-  }
-  getData(0)
+// class ProgressPromise extends Promise{
+//   constructor(executor) {
+//     const notifyHandlers = []
+//     super((resolve, reject) => {
+//        return executor(resolve, reject, progress => {
+//         notifyHandlers.forEach(notify => {
+//           notify(progress)
+//         })
+//       })
+//     })
+//     this.notifyHandlers = notifyHandlers
+//   }
+//   notify = function (fun) {
+//     this.notifyHandlers.push(fun)
+//     return this
+//   }
+// }
+// const p = new ProgressPromise((resolve, reject, notify) => {
+//   function getData(step) {
+//     if (step < 5) {
+//       notify(`${step * 20} %.........`)
+//       setTimeout(getData, 1000, step + 1)
+//     } else {
+//       resolve('done')
+//     }
+//   }
+//   getData(0)
+// })
+// p.notify(progress => {
+//   console.log(progress)
+// })
+// p.then(res => {
+//   console.log(res)
+// })
+// console.log(Promise.resolve(Promise.reject('err')))
+// async function foo() {
+//   const origin = 'xz'
+//   const obj = { }
+//   const thenable = {
+//     then(callback) {
+//       callback('thenable')
+//     }
+//   }
+//   const p = Promise.resolve('resolve')
+//   console.log(await origin, await obj, await thenable, await p)
+//   await Promise.reject('reject')
+//   console.log('end') // 不执行
+// }
+// foo().catch(console.log)
+// try {
+//     Promise.reject(1)
+// } catch (error) {
+//     console.log(error) // 捕获不到，会报错
+// }
+
+// async function foo() {
+//   console.log(await Promise.resolve('foo'))
+// }
+// async function bar() {
+//   console.log(await 'bar')
+// }
+// async function baz() {
+//   console.log('baz')
+// }
+// foo()
+// bar()
+// baz()
+
+// async function foo(){
+//   console.log(await Promise.resolve(10))
+// }
+// async function fun(){
+//   console.log(await 1)
+// }
+// foo()
+// fun()
+
+//sleep
+// function sleep(delay){
+//   return new Promise(resolve => setTimeout(resolve, delay))
+// }
+// async function main(){
+//   console.log(1)
+//   await sleep(1000)
+//   console.log(2)
+// }
+// main()
+
+// async function randomDelay(id) {
+//   // 延迟 0~1000 毫秒
+//   const delay = Math.random() * 1000;
+//   return new Promise((resolve) => setTimeout(() => {
+//   console.log(`${id} finished`);
+//   resolve();
+//   }, delay));
+//  }
+//  async function foo() {
+//   const t0 = Date.now();
+//   const promises = Array(5).fill(null).map((_, i) => randomDelay(i));
+//   console.log(`${Date.now() - t0}ms elapsed`);
+//  }
+//  foo();
+const p = new Promise((resolve, reject) => {
 })
-p.notify(progress => {
-  console.log(progress)
-})
-p.then(res => {
-  console.log(res)
-})
+const p2 = p.then(
+)
+console.log(p === p2)
